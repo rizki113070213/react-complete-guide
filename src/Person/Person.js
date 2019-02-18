91b63c4ac604862f5a47e456ecf3fa6ea4445b3f
rizki113070213 <rizki113070213@gmail.com>
2019-02-18 17:00:55 +0700

    adding styling with stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react"
+import "./Person.css"
 
 const person = (props) => {
   return (
-    <div onClick={props.click}>
+    <div onClick={props.click} className="Person">
       <h1>I'm {props.name} and I'm {props.age} years old!</h1>
       <p>{props.children}</p>
       {props.name === "Ruli" || props.name === "Fakhrul" || props.name === "Oji" || props.name === "Mita" ?
